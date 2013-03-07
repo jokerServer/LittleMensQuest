60b0621d34825f7f421d481c495f18c61a7ccfd2
Eisfreak7 <errragon@yahoo.de>
2013-03-07 17:47:35 +0100

    IchBinAwesome

--- src/LittleMensQuest.java
+++ src/LittleMensQuest.java
@@ -2,6 +2,6 @@
 public class LittleMensQuest {
 	public LittleMensQuest() {
 		System.out.println("Bananen sind gruen");
-		// UND leckere Bananen sind gelb! 123
+		// UND leckere Bananen sind gelb! 1234
 	}
 }
