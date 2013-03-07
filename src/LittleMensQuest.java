b6155193a69935b2a1dbc4038a0206fc9da47c93
Markus <Markus@jokerServer>
2013-03-07 17:38:56 +0100

    123123123123

--- src/LittleMensQuest.java
+++ src/LittleMensQuest.java
@@ -2,6 +2,6 @@
 public class LittleMensQuest {
 	public LittleMensQuest() {
 		System.out.println("Bananen sind gruen");
-		// UND leckere Bananen sind gelb!
+		// UND leckere Bananen sind gelb! 123
 	}
 }
