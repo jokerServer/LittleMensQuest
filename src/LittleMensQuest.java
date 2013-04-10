030e7337d07125fb7147ded0629cfa6087914bbc
Timo <Eisfreak7@gmail.com>
2013-04-10 23:03:13 +0200

    Set up starting class

--- src/LittleMensQuest.java
+++ src/LittleMensQuest.java
@@ -1,4 +1,7 @@
 public class LittleMensQuest {
+	public static void main(String args[]){
+		new LittleMensQuest();
+	}
 	public LittleMensQuest() {
 		System.out.println("Bananen sind gruen, SO SCHLAMPE!"); // 123121231234
 	}
