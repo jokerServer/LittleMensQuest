bbdb8ad6a7e258f64ef16011aa6bed864cd3ac19
Markus <Markus@jokerServer>
2013-04-11 21:51:44 +0200

    Add Class Equipment with a few stat variables

---
+++ src/Equipment.java
@@ -0,0 +1,14 @@
+public class Equipment {
+	private int stat_Dmg;
+	private int stat_MagicDmg;
+	private int stat_Armor;
+	private int stat_MagicResistance;
+	private int stat_LP;
+	private int stat_MP;
+	private int stat_Lifereg;
+	private int stat_Manareg;
+	private int stat_vitality; 	// Life
+	private int stat_strenght;	// Melee Dmg +	// Melee Physical Skills Dmg+
+	private int stat_magic;		// MagicDmg +	// Magical Skill Dmg+
+	private int stat_agility;	// Attackspeed+ // Ranged Physical Skills Dmg+
+}
