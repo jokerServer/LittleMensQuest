d2fed8f7299175a07fb2c5681f6b5b11cfffe5db
Timo <Eisfreak7@gmail.com>
2013-04-11 20:54:22 +0200

    Improve ability

--- src/Ability.java
+++ src/Ability.java
@@ -1,8 +1,29 @@
 
-public class Ability {
+public abstract class Ability {
+	private int id;
+	private String name;
+	
+	protected Ability(int id, String name) {
+		setId(id);
+		setName(name);
+	}
+	
+	public abstract boolean cast(Entity target);
 
-	public Ability() {
-		// TODO Auto-generated constructor stub
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
