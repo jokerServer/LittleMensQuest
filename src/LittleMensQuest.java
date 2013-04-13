ea2eb988eb144c4129fb828bd0220c6f60996a69
Timo <Eisfreak7@gmail.com>
2013-04-14 01:57:49 +0200

    Add Inventory

--- src/LittleMensQuest.java
+++ src/LittleMensQuest.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class LittleMensQuest implements KeyListener {
 	public static final int ticksPerSecond = 60;
 	private Renderer renderer = new Renderer(this);
-	private PlayerEntity ownPlayer = new PlayerEntity(1.5, 1.5, 1.5);
+	private PlayerEntity ownPlayer = new PlayerEntity(1.5, 0, 1.5, null, null, null, null, null, null);
 
 	public static void main(String args[]) {
 		new LittleMensQuest();
