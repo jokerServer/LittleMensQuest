45dd4c96549c4ac7eb1d408d0b784ecedb2ced56
Markus <Markus@jokerServer>
2013-04-14 00:45:49 +0200

    Added Toss Method to Throwable Class

--- src/Throwable.java
+++ src/Throwable.java
@@ -1,20 +1,35 @@
 import java.awt.Image;
 
 public class Throwable extends Entity{
+	private double fancyWeight;
+	private Image fancyIcon;
+	private double throwStartSpeed;
+	private long tossStart;
+	private double tossStrength;
+	
 	public Throwable(double xPosition, double yPosition, double zPosition) {
 		super(xPosition, yPosition, zPosition);
 		// TODO Auto-generated constructor stub
 	}
-	private double fancyWeight;
-	private Image fancyIcon;
 	@Override
 	protected void updateSpeed() {
-		// TODO Auto-generated method stub
-		
+		setySpeed(tossStrength - 0.00981
+				* (System.currentTimeMillis() - tossStart));
 	}
 	@Override
 	public boolean checkForCollision(Entity e) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	public void toss(int direction, int tossStrength) {
+		this.tossStrength = tossStrength;
+		if (direction==1) { // Links
+			tossStart = System.currentTimeMillis();
+			setxSpeed(tossStrength*(-1));
+		} else if (direction==2)  { // Rechts
+			tossStart = System.currentTimeMillis();
+			setxSpeed(tossStrength);
+		}
 	}
 }
